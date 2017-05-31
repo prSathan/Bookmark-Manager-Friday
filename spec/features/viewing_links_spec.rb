4c06922e3f24b168419bce9ef0042e6bb6acb6ea
Guillermo <mrog0@outlook.com>
2017-05-31 15:04:56 +0100

    Add viewing link feature test

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,7 +1,11 @@
 feature 'Viewing saved links' do
 
-  scenario 'See a list of links on the homepage' do
-    visit('/')
-    expect(page).to have_content('It works')
+  scenario 'I can see a list of links' do
+    Link.create(url: 'www.url.com', name: 'url')
+    visit('/links')
+
+    # sanity check
+    expect(page.status_code).to eq 200
+    expect(page).to have_content('www.url.com')
   end
 end
