8bec426651b1089d0c632eced84d4a618a6a3910
Prabu Sathan <prabu.sathan@gmail.com>
2017-06-01 11:57:16 +0100

    Passing first feature test

--- app/app.rb
+++ app/app.rb
@@ -3,13 +3,9 @@ require_relative '../lib/link.rb'
 
 class BookmarkManager < Sinatra::Base
 
-  get '/' do
-
-  end
-
   get '/links' do
     @links = Link.all
-    erb(':links/index')
+    erb(:'links/index')
   end
 
 end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -4,11 +4,11 @@ feature 'Viewing saved links' do
 
   scenario 'I can see a list of links' do
 
-    Link.create(url: 'http://www.url.com', title: 'url')
+    Link.create(url: 'http://www.google.co.uk', title: 'google')
     visit('/links')
 
     # sanity check
     expect(page.status_code).to eq 200
-    expect(page).to have_content('http://www.url.com')
+    expect(page).to have_content('http://www.google.co.uk')
   end
 end
