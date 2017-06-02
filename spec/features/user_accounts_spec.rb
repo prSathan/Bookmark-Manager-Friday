a9b464ed76eb66f27730e319c7ee65f97bb38079
Prabu Sathan <prabu.sathan@gmail.com>
2017-06-02 15:42:31 +0100

    Password confirmation

--- spec/features/user_accounts_spec.rb
+++ spec/features/user_accounts_spec.rb
@@ -5,4 +5,8 @@ feature 'user signup' do
     expect(page).to have_content 'Welcome student@makersacademy.com'
     expect(User.first.email).to eq('student@makersacademy.com')
   end
+
+  scenario 'user must confirm password when signing up' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,7 +1,10 @@
-def sign_up
+def sign_up(email: 'student@makersacademy.com',
+            password: 'learn',
+            password_confirmation: 'learn')
   visit '/users/new'
   expect(page.status_code).to eq(200)
-  fill_in :email,    with: 'student@makersacademy.com'
-  fill_in :password, with: 'learn!'
+  fill_in :email,    with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Signup'
 end
